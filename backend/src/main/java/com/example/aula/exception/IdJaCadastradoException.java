fed32ac5ec2af3933ce0ff9f033bb1919eee5af2
Débora <168920480+Debs2Dev@users.noreply.github.com>
2025-06-03 19:07:49 -0300

    Delete backend directory

--- backend/src/main/java/com/example/aula/exception/IdJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class IdJaCadastradoException extends RuntimeException {
-    public IdJaCadastradoException(String message) {
-        super(message);
-    }
-}
